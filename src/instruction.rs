2cae3b2f2f1d275ef93a59d9a62036e7586f33ba
hspadim <henrique@spadim.com.br>
2022-02-14 17:09:58 -0300

    FIXBUG: fixed method name

--- src/instruction.rs
+++ src/instruction.rs
@@ -37,7 +37,7 @@ impl EscrowInstruction {
     fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
         let amount = input
             .get(..8)
-            .and_then(|slice| slice.tryinto().ok())
+            .and_then(|slice| slice.try_into().ok())
             .map(u64::from_le_bytes)
             .ok_or(InvalidInstruction)?;
         Ok(amount)
